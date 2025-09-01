e3d372daa6f6b1ee7ceb4b7bb085d342a4485af2
ankitNegiDev <ankitnegi9104@gmail.com>
2025-09-01 11:45:14 +0530

    added the frontned deployed link in backend

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ const app=express();
 
 // using cors
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://cloud-drive-project-frontend-ioyp.vercel.app",
     Credentials: true
 }));
 
